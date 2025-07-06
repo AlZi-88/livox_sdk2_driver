87be29ca72ad160498f8689366856577f397bf31
Alexander Zimmerer <67343591+AlZi-88@users.noreply.github.com>
2025-07-06 20:27:27 +0200

    Missing header added

--- include/livox_sdk2_driver/livox_mid360.hpp
+++ include/livox_sdk2_driver/livox_mid360.hpp
@@ -5,6 +5,7 @@
 #include "sensor_msgs/msg/imu.hpp"
 #include "tf2_ros/static_transform_broadcaster.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2/LinearMath/Quaternion.h"
 // #include Livox SDK headers
 #include "livox_lidar_def.h"
 #include "livox_lidar_api.h"
